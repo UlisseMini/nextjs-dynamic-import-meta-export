d5caeddd93b7d5d89602d53d28d9ae70c850b402
Ulisse Mini <ulisse.mini@gmail.com>
2022-01-25 17:00:12 -0500

    Reproduce problem

---
+++ pages/foo.js
@@ -0,0 +1,19 @@
+import { genGetStaticProps } from "../lib/genGetStaticProps";
+import Link from "next/link";
+
+const meta = {
+  title: "Foo",
+};
+
+export const getStaticProps = genGetStaticProps(meta);
+
+export default function FooPage(props) {
+  return (
+    <>
+      <pre>props: {JSON.stringify(props, null, 2)}</pre>
+      <Link href="/">
+        <a>back</a>
+      </Link>
+    </>
+  );
+}
